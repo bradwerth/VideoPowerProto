f588f44452640bf886e9c1f4efc86255fe90739e
Brad Werth <werth@efn.org>
2021-06-08 16:04:40 -0700

    Initial Commit

---
+++ video-low-power/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  video-low-power
+//
+//  Created by Brad Werth on 6/8/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ video-low-power/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  video-low-power
+//
+//  Created by Brad Werth on 6/8/21.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+  // Insert code here to tear down your application
+}
+
+
+@end
